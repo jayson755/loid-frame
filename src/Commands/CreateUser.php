729197e3bf4082b142c86e689fe02019f0bf745b
jiang7552261@163.com <jiang7552261@163.com>
2018-07-25 16:11:19 +0800

    添加命令创建用户

--- src/Commands/CreateUser.php
+++ src/Commands/CreateUser.php
@@ -4,6 +4,10 @@ namespace Loid\Frame\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Database\Console\Migrations\MigrateCommand;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 use DB;
 
 class CreateUser extends Command
@@ -13,7 +17,7 @@ class CreateUser extends Command
      *
      * @var string
      */
-    protected $signature = 'loid:user {option} {username} {password} {--super-admin}';
+    protected $signature = 'loid:user {option} {name} {email} {password} {--super-admin}';
 
     /**
      * The console command description.
@@ -29,20 +33,76 @@ class CreateUser extends Command
      * @return mixed
      */
     public function handle(){
-        if ($this->argument('option') == 'create') {
-            $this->createUser();
-        } elseif ($this->argument('option') == 'update') {
-            $this->updateUser();
-        } else {
-            exit('未扩展');
+        try {
+            if ($this->argument('option') == 'create') {
+                $this->createUser($this->getArgument());
+            } elseif ($this->argument('option') == 'update') {
+                $this->updateUser();
+            } else {
+                throw new \Exception('未扩展');
+            }
+        } catch (\Exception $e) {
+            echo "【{$this->description}】错误：{$e->getMessage()}";
         }
+
     }
-    
-    private function createUser(){
-        
+
+    /**
+     * @return array
+     */
+    private function getArgument(){
+        return [
+            'name' => $this->argument('name'),
+            'email' => $this->argument('email'),
+            'password' => $this->argument('password'),
+            'password_confirmation' => $this->argument('password')
+        ];
     }
-    
+
+    /**
+     * @param array $data
+     * @throws \Exception
+     */
+    private function createUser(array $data){
+        if (DB::table('users')->count()) throw new \Exception('超级管理员已存在');
+        $this->validator($data)->validate();
+        new Registered($this->create($data));
+    }
+
+    /**
+     * @throws \Exception
+     */
     private function updateUser(){
-        
+        throw new \Exception('未扩展');
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    private function create(array $data)
+    {
+        return User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    private function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
     }
 }
